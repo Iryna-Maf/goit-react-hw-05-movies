14f65ade3fd76542f6e38f4ec143db410554eb06
Iryna-Maf <100190659+Iryna-Maf@users.noreply.github.com>
2022-09-03 11:56:48 +0300

    done Menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+
+// import Home from 'path/to/pages/Home';
+import NotFound from '../Pages/NotFound/NotFound';
+
+import s from './app.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <header>
+        <nav className={s.nav}>
+          <NavLink to="/" className={s.menuLink}>
+            Home
+          </NavLink>
+          <NavLink to="/movies" className={s.menuLink}>
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+      <hr className={s.breakLine} />
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
